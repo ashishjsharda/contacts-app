dd6f97a57b1aa977a824fd2a83aa754e06fe8302
Ashish Sharda <ashishjsharda@gmail.com>
2022-08-07 10:17:39 -0500

    Add getContactsByName

--- src/main/java/com/example/contactapp/ContactController.java
+++ src/main/java/com/example/contactapp/ContactController.java
@@ -1,6 +1,8 @@
 package com.example.contactapp;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
@@ -8,4 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContactController {
 
     private ContactRepository contactRepository;
+
+    @GetMapping("/contacts")
+    public ResponseEntity<Iterable<Contact>> getAllContacts() {
+        return ResponseEntity.ok(contactRepository.findAll());
+    }
+
+    @GetMapping("/contacts/contains/{name}")
+    public ResponseEntity<Iterable<Contact>> getContactsByName(String name) {
+        return ResponseEntity.ok(contactRepository.findByNameContainsIgnoreCase(name));
+    }
 }
